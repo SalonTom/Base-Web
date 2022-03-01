692df3494eeb9cdd2f3dbaa91f13cb3a99a07c86
Tom Salon <tsalon@oslo.fr>
2022-03-01 15:01:34 +0100

    tsmanips

--- typescript/dist/main.js
+++ typescript/dist/main.js
@@ -1,5 +1,116 @@
 "use strict";
-function hello(compiler) {
-    return "Hello from ".concat(compiler);
-}
-console.log(hello("TypeScript"));
+var manips;
+(function (manips) {
+    // function hello(compiler: string) : string {
+    //   return `Hello from ${compiler}`
+    // }
+    // console.log(hello("Typescript"))
+    var _a;
+    var age1;
+    var name1 = null;
+    var Color;
+    (function (Color) {
+        Color[Color["White"] = 0] = "White";
+        Color[Color["Black"] = 1] = "Black";
+        Color[Color["Brown"] = 2] = "Brown";
+        Color[Color["Red"] = 3] = "Red";
+        Color[Color["Orange"] = 4] = "Orange";
+        Color[Color["Yellow"] = 5] = "Yellow";
+        Color[Color["Green"] = 6] = "Green";
+        Color[Color["Blue"] = 7] = "Blue";
+        Color[Color["Purple"] = 8] = "Purple";
+        Color[Color["Grey"] = 9] = "Grey";
+    })(Color || (Color = {}));
+    var colBackground = Color.Blue;
+    var PropKind;
+    (function (PropKind) {
+        PropKind[PropKind["Read"] = 1] = "Read";
+        PropKind[PropKind["Write"] = 4] = "Write";
+        PropKind["Indexed"] = "indexed";
+    })(PropKind || (PropKind = {}));
+    var kind = PropKind.Read;
+    console.log(kind, PropKind[PropKind.Read]); // 1 Read
+    console.log(PropKind.Write, PropKind[PropKind.Write]); // 4 Write
+    console.log(PropKind.Indexed); // indexed
+    var key = Symbol();
+    var key2 = Symbol();
+    // console.log(key == key2) // erreur
+    var KEY = "key";
+    var e1 = KEY;
+    var jean = {
+        name: "Jean NEIGE",
+        age: 24,
+        printName: function () {
+            console.log("hello " + this.name);
+        },
+        printNameLambda: function () { console.log("test"); }
+    };
+    jean.printName();
+    jean.printNameLambda();
+    function hello4(named) {
+        console.log("hello " + named.name);
+    }
+    var jean3 = { name: "Jean NEIGE", age: 24 };
+    hello4(jean3); // pas d'erreur même si age n'hérite pas explicitement de Named
+    var o = { firstName: "Jean", age: 24 };
+    function addEvtListener(e, f) {
+        console.log("handler added for ".concat(e, " event"));
+    }
+    function anEventHandler(e) {
+        if (e == "click") {
+            console.log("click handled");
+        }
+        else if (e == KEY) {
+            console.log("key handled");
+        }
+    }
+    addEvtListener("click", anEventHandler);
+    addEvtListener(KEY, anEventHandler);
+    function notifyEvent(e, f) {
+        f(e);
+    }
+    var wordIndex = {
+        "interface": [28, 75, 152],
+        "class": [30, 102, 215],
+        "length": 3
+    };
+    var str = "name";
+    // const str : keyof ICharacter = "test"; throm un erreur puisque test n'est pas une clé de l'interface ICharacter
+    var jean4 = { name: "Jean NEIGE", age: 24 }; // syntaxe nom
+    var jean5 = { 'name': "Jean NEIGE", age: 24 }; // syntaxe chaîne quotes
+    var jean6 = { "name": "Jean NEIGE", age: 24 }; // syntaxe chaîne guillemets
+    var jean7 = (_a = {}, _a[str] = "Jean NEIGE", _a.age = 24, _a); // syntaxe index
+    console.log(jean4);
+    var rect = { h: 100, w: 100, scale: 1.1 };
+    var o1 = { fullName: "Jean NEIGE", age: 24, mood: "happy" };
+    console.log(o1);
+    var fullName = o1.fullName, age = o1.age; // <==> let fullName = o1.name; let age = o1.age
+    console.log(fullName, age);
+    function printAll(_a) {
+        var fullName = _a.fullName, age = _a.age;
+        console.log(fullName, age);
+    }
+    printAll(o1);
+    var unTuple = ["Jean", 24, "happy"];
+    var tuple2 = ["Ned", 55];
+    var a = unTuple[0], b = unTuple[1];
+    function printTuple(_a) {
+        var fullName = _a[0], age = _a[1];
+        console.log(fullName, age);
+    }
+    //printTuple(unTuple)
+    printTuple(tuple2);
+    var tab = [3, 5, 7, 9, 11];
+    var c = tab[0], d = tab[1];
+    var e = tab[0], f = tab[1], g = tab.slice(2); // g reçoit le reste du tableau
+    function printArray1(_a) {
+        var e = _a[0], f = _a[1];
+        console.log(e, f);
+    }
+    printArray1(tab);
+    function printArray2(_a) {
+        var e = _a[0], f = _a[1], g = _a.slice(2);
+        console.log(e, f, g.length);
+    }
+    printArray2(tab);
+})(manips || (manips = {}));
--- typescript/src/main.ts
+++ typescript/src/main.ts
@@ -1,4 +1,159 @@
-function hello(compiler: string) : string {
-  return `Hello from ${compiler}`
+namespace manips {
+  // function hello(compiler: string) : string {
+  //   return `Hello from ${compiler}`
+  // }
+  // console.log(hello("Typescript"))
+
+  let age1 : number | undefined;
+  let name1 : string | null = null;
+
+  enum Color {
+    White, Black, Brown, Red, Orange, Yellow, Green, Blue, Purple, Grey
+  }
+  let colBackground: Color = Color.Blue;
+
+  enum PropKind {
+    Read = 1,
+    Write = 4,
+    Indexed = "indexed",
+  }
+  let kind: PropKind = PropKind.Read;
+
+  console.log(kind, PropKind[PropKind.Read]); // 1 Read
+  console.log(PropKind.Write, PropKind[PropKind.Write]); // 4 Write
+  console.log(PropKind.Indexed); // indexed
+
+  const key = Symbol();
+  const key2 = Symbol();
+  // console.log(key == key2) // erreur
+
+  const KEY = "key";
+  type key_t = "key";
+  type click_t = "click";
+  type event_t = click_t | key_t;
+  let e1: event_t = KEY;
+
+  let jean = {
+    name : "Jean NEIGE",
+    age: 24,
+    printName : function() {
+        console.log("hello " + this.name);
+    },
+  printNameLambda : () => {console.log("test");}
+  }
+  jean.printName();
+  jean.printNameLambda();
+
+  interface ICharacter {
+    name: string
+    age: number
+  }
+  interface INamed {
+    name: string
+  }
+  interface Named2 {
+    name: string,
+    hello: (named: Named2) => void;
+  } 
+  function hello4(named: INamed) {
+    console.log("hello " + named.name)
+  }
+  let jean3: ICharacter = { name: "Jean NEIGE", age: 24}
+  hello4(jean3) // pas d'erreur même si age n'hérite pas explicitement de Named
+  let o = { firstName : "Jean", age : 24 }
+  // hello4(o) // erreur: propriété name manquante
+
+  interface EventHandler {
+    (e: event_t) : void
+  }
+
+  function addEvtListener(e: event_t, f: EventHandler ) {
+    console.log(`handler added for ${e} event`);
+  }
+
+  function anEventHandler(e: event_t): void {
+    if (e == "click") {
+        console.log("click handled")
+    } else if (e == KEY) {
+        console.log("key handled")
+    } 
+  }
+
+  addEvtListener("click", anEventHandler);
+  addEvtListener(KEY, anEventHandler);
+
+  function notifyEvent(e: event_t, f: EventHandler): void {
+    f(e) 
+  }
+
+  // interface WordIndex {
+  //   [word: string]: Array<number>
+  // }
+
+  // let wordIndex: WordIndex = {
+  //   "interface" : [28, 75, 152],
+  //   "class" : [30, 102, 215]
+  // }
+
+  interface WordIndex {
+    [word: string]: Array<number> | number;
+  }
+  let wordIndex: WordIndex = {
+      "interface" : [28, 75, 152],
+      "class" : [30, 102, 215],
+      "length": 3,
+  }
+
+  const str = "name"
+  // const str : keyof ICharacter = "test"; throm un erreur puisque test n'est pas une clé de l'interface ICharacter
+  let jean4: ICharacter = { name : "Jean NEIGE", age: 24};		// syntaxe nom
+  let jean5: ICharacter = { 'name' : "Jean NEIGE", age: 24};	// syntaxe chaîne quotes
+  let jean6: ICharacter = { "name" : "Jean NEIGE", age: 24};	// syntaxe chaîne guillemets
+  let jean7: ICharacter = { [str] : "Jean NEIGE", age: 24};	// syntaxe index
+
+  console.log(jean4);
+
+  interface Shape {
+    h: number;
+    w: number;
+  }
+
+  interface Shape {
+    scale: number;
+  }
+
+  const rect: Shape = { h: 100, w: 100, scale: 1.1 };
+
+  interface ICharacter1 { fullName: string, age: number }
+  let o1 = { fullName: "Jean NEIGE", age: 24, mood: "happy" }
+  console.log(o1);
+  let { fullName, age } = o1 // <==> let fullName = o1.name; let age = o1.age
+  console.log(fullName, age)
+  function printAll({ fullName, age }: ICharacter1) {
+      console.log(fullName, age)
+  }
+  printAll(o1)
+
+  let unTuple: [string, number, string] = ["Jean", 24, "happy"]
+  let tuple2: [string, number] = ["Ned", 55]
+  let [a, b] = unTuple;
+  function printTuple([fullName, age]: [string, number]) {
+      console.log(fullName, age)
+  }
+  //printTuple(unTuple)
+  printTuple(tuple2)
+
+  let tab: Array<number> = [3, 5, 7, 9, 11]
+  let [c, d] = tab
+  let [e, f, ...g] = tab // g reçoit le reste du tableau
+
+  function printArray1([e, f]: Array<number>) {
+      console.log(e, f)
+  }
+  printArray1(tab)
+  function printArray2([e, f, ...g]: Array<number>) {
+      console.log(e, f, g.length)
+  }
+  printArray2(tab);
+
 }
-console.log(hello("TypeScript"))
